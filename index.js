d07303bdcb12ea96edb8bcd51c116038e0502eef
kaigara334 <kai201010@outlook.jp>
2024-11-03 14:20:56 +0900

    index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const {
 } = require("discord.js");
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // サーバーを起動してスリープを防ぐ
 app.get("/", (req, res) => res.send("Bot is running"));
